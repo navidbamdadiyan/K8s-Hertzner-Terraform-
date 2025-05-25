1605f93c7ee2d9d2bdac02f3c3756eafc44b7862
navidbamdadiyan <130226904+navidbamdadiyan@users.noreply.github.com>
2025-05-25 16:23:47 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,31 +3,19 @@ provider "hcloud" {
 }
 
 resource "hcloud_ssh_key" "default" {
-  name      = "my-ssh-key"
+  name       = "k8s-key"
   public_key = file(var.ssh_public_key)
 }
 
-resource "hcloud_server" "k8s_master" {
-  name        = "k8s-master"
+resource "hcloud_server" "k8s_nodes" {
+  count       = 3
+  name        = "k8s-node-${count.index + 1}"
   image       = "ubuntu-22.04"
   server_type = "cx31"
   location    = var.location
   ssh_keys    = [hcloud_ssh_key.default.id]
 }
 
-resource "hcloud_server" "k8s_worker" {
-  count       = var.worker_count
-  name        = "k8s-worker-${count.index + 1}"
-  image       = "ubuntu-22.04"
-  server_type = "cx21"
-  location    = var.location
-  ssh_keys    = [hcloud_ssh_key.default.id]
-}
-
-output "master_ip" {
-  value = hcloud_server.k8s_master.ipv4_address
-}
-
-output "worker_ips" {
-  value = [for server in hcloud_server.k8s_worker : server.ipv4_address]
+output "node_ips" {
+  value = [for s in hcloud_server.k8s_nodes : s.ipv4_address]
 }
